a5f17db2ad8f55fe2ed9659801a1ba3201287631
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-07-07 10:01:55 +0100

    Add a global error handler

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, World!"));
 
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
