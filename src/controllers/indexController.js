fac631fe55513875dd28e75fc914f76bf5f30d69
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-08-10 01:40:37 +0100

    Add a catch-all route to handle 404 errors

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,1 +1,7 @@
 export const renderHomePage = (req, res) => res.render("index");
+
+export const render404Page = (req, res) => {
+  res
+    .status(404)
+    .render("error", { message: "Page Not Found", statusCode: 404 });
+};
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { renderHomePage } from "../controllers/indexController.js";
+import {
+  renderHomePage,
+  render404Page,
+} from "../controllers/indexController.js";
 
 const indexRouter = Router();
 
 indexRouter.get("/", renderHomePage);
+
+indexRouter.get("/*splat", render404Page);
 
 export default indexRouter;
